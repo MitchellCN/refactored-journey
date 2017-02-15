d86744faf0d95aa84dff5b9da2093bb03be651af
RoganRhysShane <cmitchell@Chriss-MBP.lan>
2017-02-15 11:29:06 -0500

    Newline
    bankaccount

---
+++ Bankaccount.java
@@ -0,0 +1,23 @@
+// Implement the TODOs inside the BankAccount class
+ class BankAccount {
+  Integer accountBalance = 0;
+
+  public class Main{
+  public void depositFunds(Integer amount){
+    BankAccount bankAccountOne = new BankAccount();
+    bankAccountOne.balance = 300;
+    System.out.println(bankAccountOne.balance);
+  	// TODO: increase the property `accountBalance` by the `amount`
+   }
+  }
+  public void withdrawFunds(Integer amount){
+    BankAccount bankAccountTwo = new BankAccount();
+    bankAccountTwo.balance = -200;
+    System.out.println(bankAccountTwo.balance);
+  	// TODO: decrease the property `accountBalance` by the `amount`
+
+
+
+System.out.printlin(bankAccountOne.balance + bankAccountTwo.balance);
+   }
+}
